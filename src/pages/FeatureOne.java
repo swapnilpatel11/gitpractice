c6448c31eeb77eb2e91e7345844c63a9b8ac49a5
swapnilpatel11 <sappu004@gmail.com>
2022-02-24 20:12:57 -0500

    feature two added

--- src/pages/FeatureOne.java
+++ src/pages/FeatureOne.java
@@ -5,6 +5,7 @@ public class FeatureOne {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Feature one");
+		System.out.println("Feature two");
 	}
 
 }
